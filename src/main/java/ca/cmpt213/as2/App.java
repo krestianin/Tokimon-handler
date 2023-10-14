c54667bf08981d3071cfdec9bf23e89cf6b5acce
krestianin <ivanpostolov03@mail.ru>
2023-10-14 12:55:38 -0700

    init commit

---
+++ src/main/java/ca/cmpt213/as2/App.java
@@ -0,0 +1,13 @@
+package ca.cmpt213.as2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
